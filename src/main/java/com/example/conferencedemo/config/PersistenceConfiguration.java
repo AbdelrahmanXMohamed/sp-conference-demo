03f48d165df078741b0a524d28a759febf2c9e4b
AbdelrahmanXMohamed <abdelrahman.mohamedx1999@gmail.com>
2022-11-13 13:54:42 +0200

    setup database

--- src/main/java/com/example/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/example/conferencedemo/config/PersistenceConfiguration.java
@@ -7,21 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    DataSource dataSource()
-    {
-        DataSourceBuilder dsBuilder = DataSourceBuilder.create();
-//
-        dsBuilder.url ("jdbc:postgresql://localhost:5432/conference_app");
-        dsBuilder.username ("postgres");
-        dsBuilder.password ("01005883275");
-        System.out.println("am here");
-        return (DataSource) dsBuilder.build();
-    }
-//    public DataSource dataSource()
+//    @Bean
+//    DataSource dataSource()
 //    {
-//        DataSourceBuilder builder=DataSourceBuilder.create();
-//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-//        System.out.println("my databas");
+//        DataSourceBuilder dsBuilder = DataSourceBuilder.create();
+////
+//        dsBuilder.url ("jdbc:postgres://vshjjpnnelxcfz:d8c559a6bb57b91fecfd75af55d16e768f594400745d24f76f79881068148a21@ec2-44-195-132-31.compute-1.amazonaws.com:5432/d4i8iopgpmb7lo");
+//        dsBuilder.username ("postgres");
+//        dsBuilder.password ("01005883275");
+//        System.out.println("am here");
+//        return (DataSource) dsBuilder.build();
 //    }
+
 }
